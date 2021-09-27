1920a7ac7a54c768c70db0c9829e69261d5e8a72
Martulenka <87800213+Martulenka@users.noreply.github.com>
2021-09-27 21:30:35 +0200

    Added new java project for Expressions and Control Flow

---
+++ week-03/day-1/ExpressionsAndControlFlow/src/com/company/HelloOthers.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class HelloOthers {
+    public static void main(String[] args) {
+        // Greet 3 of your classmates with this program in three separate lines
+        // like:
+        //
+        // Hello, Esther!
+        // Hello, Mary!
+        // Hello, Joe!
+
+        System.out.println("Hello, Brianna");
+        System.out.println("Hello, Frederica");
+        System.out.println("Hello, Igor");
+
+    }
+}
