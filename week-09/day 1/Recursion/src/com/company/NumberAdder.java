626815f6b464dab566d8079c5794746e6cd0325b
Martulenka <martina.hudecek@gmail.com>
2021-11-22 21:05:24 +0100

    finished fibonacci exercise

--- week-09/day 1/Recursion/src/com/company/NumberAdder.java
+++ week-09/day 1/Recursion/src/com/company/NumberAdder.java
@@ -7,12 +7,12 @@ public class NumberAdder {
 
     public static void main(String[] args) {
 
-        System.out.println (AddNumber(10));
+        System.out.println (AddNumber(-10));
     }
 
     public static int AddNumber (int n) {
 
-        if (n == 1) {
+        if (n <= 1) {
             return 1;
         } else {
             return n + AddNumber(n - 1);
