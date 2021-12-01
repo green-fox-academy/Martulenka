a8be4281f4a5d3f55cda030d8e7cd2c269a091cf
Martulenka <martina.hudecek@gmail.com>
2021-12-01 20:41:53 +0100

    finished Cloneable exercise

---
+++ week-10/day 2/Interface/src/com/company/Cloneable/Main.java
@@ -0,0 +1,20 @@
+package com.company.Cloneable;
+
+public class Main {
+    public static void main(String[] args) {
+    /*
+    Cloneable
+    Every Object has a clone() method (so all of your classes will have an inherited one too), but by default it doesn't do anything.
+    Get your Student and other relevant classes from the Green Fox Organization Exercise
+    Override and implement the clone() method, so it clones the student. Cloning means: create an independent copy with the same attributes (fields).
+    Instantiate John, a 20 years old male from BME
+    Clone him into jonhTheClone
+     */
+
+        Student john = new Student("John", 20, "male", "BME");
+
+        Student jonhTheClone = john.clone();
+
+        jonhTheClone.introduce();
+    }
+}
---
+++ week-10/day 2/Interface/src/com/company/Cloneable/Student.java
@@ -0,0 +1,53 @@
+package com.company.Cloneable;
+
+public class Student extends Person {
+    /*Create a Student class that has the same fields and methods as the Person class, and has the following additional
+
+    fields:
+    previousOrganization: the name of the student’s previous company / school
+    skippedDays: the number of days skipped from the course
+    methods:
+    getGoal(): prints "Be a junior software developer."
+    introduce(): prints "Hi, I'm name, a age year old gender from previousOrganization who skipped skippedDays days from the course already."
+    skipDays(numberOfDays): increases skippedDays by numberOfDays
+    The Student class has the following constructors:
+
+    Student(name, age, gender, previousOrganization): beside the given parameters, it sets skippedDays to 0
+    Student(): sets name to Jane Doe, age to 30, gender to female, previousOrganization to The School of Life, skippedDays to 0*/
+
+    String previousOrganization;
+    int skippedDays;
+
+    public Student(){
+        super();
+        this.previousOrganization = "The School of Life";
+        this.skippedDays = 0;
+    };
+
+    public Student (String name, int age, String gender, String previousOrganization){
+        super(name, age, gender);
+        this.previousOrganization = previousOrganization;
+        this.skippedDays = 0;
+    }
+
+    public void getGoal(){
+        System.out.println("Be a junior software developer.");
+    }
+
+    public void introduce(){
+        System.out.println("Hi, I'm " + super.name + ", a "+ super.age + " year old " +  super.gender + " from " +  this.previousOrganization + " who skipped " + this.skippedDays + " days from the course already.");
+    }
+
+    public void skipDays(int numberOfDays){
+        this.skippedDays += numberOfDays;
+    }
+
+    @Override
+    public Student clone(){
+        Student clone = new Student(super.name, super.age, super.gender, previousOrganization);
+        clone.skippedDays = skippedDays;
+
+        return clone;
+    }
+
+}
