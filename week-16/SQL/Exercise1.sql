7f10c26bf9e746d47f9fe5afa2be66ec816b8c21
Martulenka <martina.hudecek@gmail.com>
2022-01-29 12:14:53 +0100

    Finished exercise 1 - Movies

--- week-16/SQL/Exercise1.sql
+++ week-16/SQL/Exercise1.sql
@@ -57,3 +57,46 @@ select distinct movie.year from movie inner join rating on movie.mID=rating.mID 
 
 -- Exercise 3
 -- Find the titles of all movies that have no ratings.
+select movie.title from movie left join rating on movie.mID=rating.mID where rating.mID is null;
+
+-- Exercise 4
+-- Some reviewers didn’t provide a date with their rating. Find the names of all reviewers who have ratings with a NULL value for the date.
+select reviewer.name from reviewer inner join rating on reviewer.rID=rating.rID where rating.ratingDate is null;
+
+-- Exercise 5
+-- Write a query to return the ratings data in a more readable format: reviewer name, movie title, stars, and ratingDate. Also, sort the data, first by reviewer name, then by movie title, and lastly by number of stars.
+select reviewer.name, movie.title, rating.stars, rating.ratingDate from reviewer inner join rating on reviewer.rID=rating.rID inner join movie on movie.mID = rating.mID order by reviewer.name, movie.title, rating.stars;
+
+-- Exercise 6
+-- For all cases where the same reviewer rated the same movie twice and gave it a higher rating the second time, return the reviewer’s name and the title of the movie.
+select reviewer.name, movie.title from movie 
+inner join rating rA using(mID)
+inner join rating rB using(rID,mID)
+inner join reviewer using(rID)
+where rA.mID = rB.mID and rA.ratingDate>rB.ratingDate and rA.stars>rB.stars;
+
+-- Exercise 7
+-- For each movie that has at least one rating, find the highest number of stars that movie received. Return the movie title and number of stars. Sort by movie title.
+select movie.title, max(rating.stars) from movie inner join rating on movie.mID = rating.mID 
+group by movie.title
+order by movie.title;
+
+-- Exercise 8
+-- For each movie, return the title and the ‘rating spread’, that is, the difference between highest and lowest ratings given to that movie. Sort by rating spread from highest to lowest, then by movie title.
+select movie.title, (max(rating.stars) - min(rating.stars)) AS rating_spread from movie inner join rating on movie.mID = rating.mID 
+group by movie.title
+order by rating_spread DESC, movie.title;
+
+-- Exercise 9
+-- Find the difference between the average rating of movies released before 1980 and the average rating of movies released after 1980. (Make sure to calculate the average rating for each movie, then the average of those averages for movies before 1980 and movies after. Don’t just calculate the overall average rating before and after 1980.)
+select avg(b41980.avg) - avg(after1980.avg) from 
+(select avg(rating.stars) as avg
+from movie
+inner join rating on movie.mID = rating.mID
+where year < 1980
+group by movie.mID) as b41980,
+(select avg(rating.stars) as avg
+from movie
+inner join rating on movie.mID = rating.mID
+where year >= 1980
+group by movie.mID) as after1980;
