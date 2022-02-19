0fe300bb6dd977b9c315e6b15a75469484bbe3f0
Martulenka <martina.hudecek@gmail.com>
2022-02-19 11:10:51 +0100

    created ListingTodosWithH2 project

---
+++ week-18/ListingTodosWithH2/src/main/java/com/example/listingtodoswithh2/ListingTodosWithH2Application.java
@@ -0,0 +1,13 @@
+package com.example.listingtodoswithh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListingTodosWithH2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListingTodosWithH2Application.class, args);
+    }
+
+}
